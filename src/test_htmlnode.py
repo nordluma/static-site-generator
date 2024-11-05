60677e94ee868bc593d6ac2aeb00a38986366cf5
nordluma <maxnordlund.95@hotmail.com>
2024-11-05 23:03:37 +0200

    fix(test_htmlnode): tags are expected to be without arrows

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -5,23 +5,21 @@ from htmlnode import HtmlNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_repr(self):
-        node = HtmlNode(tag="<a>", value="google", props={"href": "https://google.com"})
+        node = HtmlNode(tag="a", value="google", props={"href": "https://google.com"})
         self.assertEqual(
             node.__repr__(),
-            "HtmlNode(<a>, google, None, {'href': 'https://google.com'})",
+            "HtmlNode(a, google, None, {'href': 'https://google.com'})",
         )
 
     def creates_prop_string(self):
         test_cases = [
             (
-                HtmlNode(
-                    tag="<a>", value="google", props={"href": "https://google.com"}
-                ),
+                HtmlNode(tag="a", value="google", props={"href": "https://google.com"}),
                 " href=https://google.com",
             ),
             (
                 HtmlNode(
-                    tag="<a>",
+                    tag="a",
                     value="youtube",
                     props={"href": "http://youtube.com", "target": "_blank"},
                 ),
@@ -29,15 +27,15 @@ class TestHtmlNode(unittest.TestCase):
             ),
             (
                 HtmlNode(
-                    "<form>",
+                    "form",
                     None,
                     children=[
-                        HtmlNode("<input>", None, None, {"type": "text", "id": "test"})
+                        HtmlNode("input", None, None, {"type": "text", "id": "test"})
                     ],
                 ),
                 " type=text id=test",
             ),
-            (HtmlNode("<h1>", "Hello World", None, None), ""),
+            (HtmlNode("h1", "Hello World", None, None), ""),
         ]
 
         for value, expected in test_cases:
