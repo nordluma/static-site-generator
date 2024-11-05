14e8601727a2c173b6b846cbaee85ec01afd0270
nordluma <maxnordlund.95@hotmail.com>
2024-11-05 22:33:05 +0200

    test(htmlnode): `props_to_html` format props correctly

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -11,3 +11,34 @@ class TestHtmlNode(unittest.TestCase):
             "HtmlNode(<a>, google, None, {'href': 'https://google.com'})",
         )
 
+    def creates_prop_string(self):
+        test_cases = [
+            (
+                HtmlNode(
+                    tag="<a>", value="google", props={"href": "https://google.com"}
+                ),
+                " href=https://google.com",
+            ),
+            (
+                HtmlNode(
+                    tag="<a>",
+                    value="youtube",
+                    props={"href": "http://youtube.com", "target": "_blank"},
+                ),
+                " href=http://youtube.com target=_blank",
+            ),
+            (
+                HtmlNode(
+                    "<form>",
+                    None,
+                    children=[
+                        HtmlNode("<input>", None, None, {"type": "text", "id": "test"})
+                    ],
+                ),
+                " type=text id=test",
+            ),
+            (HtmlNode("<h1>", "Hello World", None, None), ""),
+        ]
+
+        for value, expected in test_cases:
+            self.assertEqual(value.props_to_html(), expected)
