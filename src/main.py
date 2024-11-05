684c47cb26fe8548e01f8aa11f1b2f66c46a4608
nordluma <maxnordlund.95@hotmail.com>
2024-11-05 23:32:37 +0200

    feat: print a `LeafNode`

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+from htmlnode import LeafNode
 from textnode import TextType, TextNode
 
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
     print(node)
+
+    leaf_node = LeafNode("h1", "hello world")
+    print(leaf_node)
+    print(leaf_node.to_html())
 
 
 if __name__ == "__main__":
