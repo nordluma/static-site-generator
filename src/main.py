6239f6e5c30f020ee0fed779b704587b86243a47
nordluma <maxnordlund.95@hotmail.com>
2024-11-08 21:08:47 +0200

    feat: print html of `ParentNode` and `LeafNode`

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from htmlnode import LeafNode
+from htmlnode import LeafNode, ParentNode
 from textnode import TextType, TextNode
 
 
@@ -6,9 +6,23 @@ def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
     print(node)
 
-    leaf_node = LeafNode("h1", "hello world")
-    print(leaf_node)
-    print(leaf_node.to_html())
+    parent_node = ParentNode(
+        tag="p",
+        children=[
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+
+    print(parent_node.to_html())
+    print("----")
+
+    leaf_node = LeafNode(
+        tag="a", value="take action", props={"href": "not.phishing.com"}
+    )
+    print(leaf_node.props_to_html())
 
 
 if __name__ == "__main__":
