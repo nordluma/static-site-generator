20931fd4aad9925a962cf988fbefa9c3b4f9d4bb
nordluma <maxnordlund.95@hotmail.com>
2024-11-08 22:08:47 +0200

    test(textnode): conversion from all possible `TextType`s to `LeafNode`

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from htmlnode import LeafNode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,6 +9,54 @@ class TestTextNode(unittest.TestCase):
         node_1 = TextNode("This is a text node", TextType.BOLD)
         node_2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node_1, node_2)
+
+
+class TestConvertTextNodeToLeafNode(unittest.TestCase):
+    def test_convert_normal_to_leafnode(self):
+        self.assertEqual(
+            text_node_to_html_node(TextNode("normal text", TextType.NORMAL)),
+            LeafNode(None, "normal text"),
+        )
+
+    def test_convert_bold_to_leafnode(self):
+        self.assertEqual(
+            text_node_to_html_node(TextNode("bold text", TextType.BOLD)),
+            LeafNode("b", "bold text"),
+        )
+
+    def test_convert_italic_to_leafnode(self):
+        self.assertEqual(
+            text_node_to_html_node(TextNode("italic text", TextType.ITALIC)),
+            LeafNode("i", "italic text"),
+        )
+
+    def test_convert_code_to_leafnode(self):
+        self.assertEqual(
+            text_node_to_html_node(TextNode("code text", TextType.CODE)),
+            LeafNode("code", "code text"),
+        )
+
+    def test_convert_link_to_leafnode(self):
+        self.assertEqual(
+            text_node_to_html_node(
+                TextNode("anchor text", TextType.LINK, "example.com")
+            ),
+            LeafNode("a", "anchor text", {"href": "example.com"}),
+        )
+
+    def test_convert_image_to_leafnode(self):
+        self.assertEqual(
+            text_node_to_html_node(TextNode("alt text", TextType.IMAGE, "image.jpg")),
+            LeafNode("img", "", {"src": "image.jpg", "alt": "alt text"}),
+        )
+
+    # We do not need this test for now since we are using type hints
+    """
+    def test_convert_invalid_texttype_to_leafnode(self):
+        self.assertRaises(
+            text_node_to_html_node(TextNode("text", TextType.Invalid, None))
+        )
+    """
 
 
 if __name__ == "__main__":
