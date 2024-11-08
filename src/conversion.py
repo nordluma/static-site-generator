561aab7364d4bfe3e6f84ec386cfe400f8be5e6b
nordluma <maxnordlund.95@hotmail.com>
2024-11-08 23:37:44 +0200

    feat(conversion): split formatted parts from a list of `TextNode`s

--- src/conversion.py
+++ src/conversion.py
@@ -19,3 +19,39 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
         case _:
             raise Exception("invalid text type for `TextNode`")
     return node
+
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        # node has already a special text type, skip it
+        if old_node.text_type != TextType.NORMAL.value:
+            new_nodes.append(old_node)
+            continue
+
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError(f"Invalid Markdown. {text_type} section not closed")
+
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            # When we split the text with the chosen delimiter the list is
+            # always even if the markdown is valid (this is checked above).
+            # This means that all elements with even index is "normal" text and
+            # elements with odd index are the ones we are looking for.
+            #
+            # Example:
+            #
+            # Index                                 0             1           2
+            # "This is a `code block` text" -> ["This is a", "code block", "text"]
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.NORMAL))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+
+    return new_nodes
