1e25e3257655465ecc64f176c71c3c27cce3288a
nordluma <maxnordlund.95@hotmail.com>
2024-12-18 23:50:10 +0200

    feat(conversion): implement `split_nodes_link`

--- src/conversion.py
+++ src/conversion.py
@@ -72,6 +72,40 @@ def split_nodes_image(old_nodes: list[TextNode]):
             new_nodes.append(TextNode(original_text, TextType.TEXT))
 
     return new_nodes
+
+
+def split_nodes_link(old_nodes: list[TextNode]):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        for alt_text, link in links:
+            sections = original_text.split(f"[{alt_text}]({link})", maxsplit=1)
+
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(alt_text, TextType.LINK, link))
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
+
+
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
